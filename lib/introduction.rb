97495f465b133447da93ed826e890846d3b41ad2
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-12 18:12:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
   
-  
-  
 p introduction("Josh")
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}. "
+end
+
+p introduction_with_language("Dan", "Ember.js")
